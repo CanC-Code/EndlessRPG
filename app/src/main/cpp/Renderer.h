7a7e06c12bf81772c7208f0c3b2c2a0b28f9eeb2
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-24 00:53:45 -0600

    Update Renderer.h

--- app/src/main/cpp/Renderer.h
+++ app/src/main/cpp/Renderer.h
@@ -1,13 +1,29 @@
 #pragma once
 #include <GLES3/gl31.h>
+#include <string>
 
 class GrassRenderer {
 private:
-    GLuint computeProgram, renderProgram, ssbo, vao, vbo;
+    GLuint computeProgram;
+    GLuint renderProgram;
+    GLuint ssbo;
+    GLuint vao, vbo;
+    
     const int GRASS_COUNT = 256 * 256; 
+
+    // Internal helpers for shader compilation and matrix math
+    GLuint compileShader(GLenum type, const std::string& source);
+    GLuint createProgram(GLuint vertexShader, GLuint fragmentShader);
+    GLuint createComputeProgram(GLuint computeShader);
+    
+    void buildPerspectiveMatrix(float* m, float fov, float aspect, float zNear, float zFar);
+    void buildLookAtMatrix(float* m, float ex, float ey, float ez, float cx, float cy, float cz);
+    void multiplyMatrix(float* out, const float* a, const float* b);
 
 public:
     GrassRenderer() : computeProgram(0), renderProgram(0), ssbo(0), vao(0), vbo(0) {}
+    ~GrassRenderer() = default;
+
     void init();
-    void updateAndRender(float time, float deltaTime);
+    void updateAndRender(float time, float deltaTime, int screenWidth, int screenHeight);
 };
