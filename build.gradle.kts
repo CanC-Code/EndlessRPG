7acefc27b9e8be8060cdf4e48df91ef8d1c5afc0
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-23 23:21:31 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
-// Top-level build file where you add configuration options common to all sub-projects.
 plugins {
-    // Defines the Android application plugin version but doesn't apply it to the root
     id("com.android.application") version "8.3.0" apply false
-    
-    // Defines the Kotlin plugin version
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
