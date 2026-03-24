d6262d063c46bb2d19659526418ad2e5fc277a31
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-23 23:24:26 -0600

    Update Renderer.h

--- app/src/main/cpp/Renderer.h
+++ app/src/main/cpp/Renderer.h
@@ -3,18 +3,11 @@
 
 class GrassRenderer {
 private:
-    GLuint computeProgram;
-    GLuint renderProgram;
-    GLuint ssbo;
-    GLuint vao, vbo;
-    
-    // 256x256 grid of grass per terrain chunk
+    GLuint computeProgram, renderProgram, ssbo, vao, vbo;
     const int GRASS_COUNT = 256 * 256; 
 
 public:
     GrassRenderer() : computeProgram(0), renderProgram(0), ssbo(0), vao(0), vbo(0) {}
-    ~GrassRenderer() = default;
-
     void init();
     void updateAndRender(float time, float deltaTime);
 };
