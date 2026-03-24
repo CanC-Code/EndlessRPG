4cbced0abe2a740fde8474d2f6ab50b1315272cd
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-24 11:24:30 -0600

    Update Renderer.h

--- app/src/main/cpp/Renderer.h
+++ app/src/main/cpp/Renderer.h
@@ -2,7 +2,6 @@
 #include <GLES3/gl31.h>
 #include <string>
 
-// 512 * 512 = 262,144 blades for a high-density field like the photo
 const int GRASS_COUNT = 262144; 
 
 class GrassRenderer {
@@ -11,15 +10,18 @@ public:
     void init();
     void updateAndRender(float time, float deltaTime, int screenWidth, int screenHeight);
 
+    // Camera variables for infinite generation
+    float camX = 0.0f;
+    float camY = 1.8f;
+    float camZ = 0.0f;
+
 private:
-    GLuint computeProgram;
-    GLuint renderProgram;
-    GLuint ssbo;
-    GLuint vao, vbo;
+    GLuint computeProgram, renderProgram;
+    GLuint ssbo, vao, vbo;
 
     GLuint compileShader(GLenum type, const std::string& source);
-    GLuint createProgram(GLuint vertexShader, GLuint fragmentShader);
-    GLuint createComputeProgram(GLuint computeShader);
+    GLuint createProgram(GLuint vShader, GLuint fShader);
+    GLuint createComputeProgram(GLuint cShader);
 
     void buildPerspective(float* m, float fov, float aspect, float zNear, float zFar);
     void buildLookAt(float* m, float ex, float ey, float ez, float cx, float cy, float cz);
