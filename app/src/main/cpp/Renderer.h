e1e8946acca00189ba2f27e8028a92025437430d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-24 10:23:13 -0600

    Update Renderer.h

--- app/src/main/cpp/Renderer.h
+++ app/src/main/cpp/Renderer.h
@@ -2,21 +2,27 @@
 #include <GLES3/gl31.h>
 #include <string>
 
+// 512 * 512 instances for a dense field
+const int GRASS_COUNT = 262144; 
+
 class GrassRenderer {
-private:
-    GLuint computeProgram, renderProgram, ssbo, vao, vbo;
-    const int GRASS_COUNT = 256 * 256; 
-
-    GLuint compileShader(GLenum type, const std::string& source);
-    GLuint createProgram(GLuint v, GLuint f);
-    GLuint createComputeProgram(GLuint c);
-    
-    void buildPerspective(float* m, float fov, float aspect, float zn, float zf);
-    void buildLookAt(float* m, float ex, float ey, float ez, float cx, float cy, float cz);
-    void multiply(float* out, const float* a, const float* b);
-
 public:
     GrassRenderer();
     void init();
-    void updateAndRender(float time, float dt, int w, int h);
+    void updateAndRender(float time, float deltaTime, int screenWidth, int screenHeight);
+
+private:
+    GLuint computeProgram;
+    GLuint renderProgram;
+    GLuint ssbo;
+    GLuint vao;
+    GLuint vbo;
+
+    GLuint compileShader(GLenum type, const std::string& source);
+    GLuint createProgram(GLuint vertexShader, GLuint fragmentShader);
+    GLuint createComputeProgram(GLuint computeShader);
+
+    void buildPerspective(float* m, float fov, float aspect, float zNear, float zFar);
+    void buildLookAt(float* m, float ex, float ey, float ez, float cx, float cy, float cz);
+    void multiply(float* out, const float* a, const float* b);
 };
