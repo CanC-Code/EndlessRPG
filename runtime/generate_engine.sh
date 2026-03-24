105400f938e9e61df5ab2ab7aab9480d55ccabe6
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-23 20:27:45 -0600

    Update generate_engine.sh

--- runtime/generate_engine.sh
+++ runtime/generate_engine.sh
@@ -12,9 +12,11 @@ cat <<EOF > $OUT
 #include "shaders/Shaders.h"
 
 GLuint prog, vH, vB, vL, vS, vSh;
-GLint uMVP, uMod, uSun, uView, uFog;
+GLint uMVP, uMod, uSun, uView;
 int sw=1, sh=1;
-float pX=0, pZ=0, vY=0, anim=0;
+
+// Physics and Animation State
+float pX=0, pZ=0, vY=0, animTime=0, attackSwing=0;
 bool grounded = true;
 
 GLuint mkVAO(const float* d, int c) {
@@ -33,33 +35,40 @@ extern "C" {
         glShaderSource(vs,1,&WORLD_VS,0); glCompileShader(vs);
         glShaderSource(fs,1,&WORLD_FS,0); glCompileShader(fs);
         prog=glCreateProgram(); glAttachShader(prog,vs); glAttachShader(prog,fs); glLinkProgram(prog);
+        
         uMVP=glGetUniformLocation(prog,"uMVP"); uMod=glGetUniformLocation(prog,"uModel");
         uSun=glGetUniformLocation(prog,"uSunDir"); uView=glGetUniformLocation(prog,"uViewPos");
-        uFog=glGetUniformLocation(prog,"uFogColor");
+        
         vH=mkVAO(M_HEAD,N_HEAD); vB=mkVAO(M_BODY,N_BODY); vL=mkVAO(M_LIMB,N_LIMB);
         vS=mkVAO(M_SWORD,N_SWORD); vSh=mkVAO(M_SHIELD,N_SHIELD);
     }
 
-    JNIEXPORT void JNICALL Java_com_game_procedural_GameLib_onDraw(JNIEnv* e, jclass c, float jX, float jY, float yaw, float pitch, bool jump, bool atk) {
-        // Physics & Animation Logic
-        if(jump && grounded) { vY = 0.25f; grounded = false; }
-        if(!grounded) { vY -= 0.015f; }
-        float pY = (grounded) ? 0.0f : (vY > -1.0f ? vY : -1.0f); // Simple Floor
-        if(pY < 0) { pY=0; grounded=true; vY=0; }
+    JNIEXPORT void JNICALL Java_com_game_procedural_GameLib_onDraw(JNIEnv* e, jclass c, float jX, float jY, float yaw, float pitch, jboolean jump, jboolean atk) {
         
+        // 1. Actions & Physics
+        if(jump && grounded) { vY = 0.35f; grounded = false; }
+        if(!grounded) { vY -= 0.02f; } // Gravity
+        float pY = (grounded) ? 0.0f : (vY > -1.0f ? vY : -1.0f);
+        if(pY <= 0) { pY=0; grounded=true; vY=0; }
+        
+        if(atk) { attackSwing = 3.14f; } // Trigger Sword Swing Arc
+        if(attackSwing > 0) attackSwing -= 0.25f; else attackSwing = 0;
+
+        // 2. Thumbstick Movement
         float speed = 0.15f;
         pX += (jX * cos(yaw) + jY * sin(yaw)) * speed;
         pZ += (jY * cos(yaw) - jX * sin(yaw)) * speed;
-        if(abs(jX)>0.1 || abs(jY)>0.1) anim += 0.2f; else anim = 0;
+        
+        if(abs(jX)>0.1 || abs(jY)>0.1) animTime += 0.2f; else animTime = 0;
 
+        // 3. Render Setup
         glClearColor(0.7f, 0.85f, 0.95f, 1.0f); glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST); glUseProgram(prog);
 
         glm::vec3 cp = glm::vec3(12.0f*cos(pitch)*sin(yaw)+pX, 8.0f+pY, 12.0f*cos(pitch)*cos(yaw)+pZ);
         glm::mat4 v = glm::lookAt(cp, {pX, 1.5f+pY, pZ}, {0,1,0});
         glm::mat4 pr = glm::perspective(0.8f, (float)sw/sh, 0.1f, 100.0f);
-        glUniform3f(uSun, 1, 2, 1); glUniform3f(uView, cp.x, cp.y, cp.z);
-        glUniform3f(uFog, 0.7f, 0.85f, 0.95f);
+        glUniform3f(uSun, 1.0f, 2.0f, 1.0f); glUniform3f(uView, cp.x, cp.y, cp.z);
 
         auto dr = [&](GLuint vao, int n, glm::mat4 m) {
             glUniformMatrix4fv(uMod, 1, 0, glm::value_ptr(m));
@@ -67,19 +76,24 @@ extern "C" {
             glBindVertexArray(vao); glDrawArrays(GL_TRIANGLES, 0, n);
         };
 
+        // 4. Character Assembly with Animations
         glm::mat4 root = glm::translate(glm::mat4(1), {pX, pY, pZ});
-        dr(vB, N_BODY, glm::translate(root, {0, 1.2, 0}));
-        dr(vH, N_HEAD, glm::translate(root, {0, 2.2, 0}));
+        dr(vB, N_BODY, glm::translate(root, {0, 1.2, 0})); // Body
+        dr(vH, N_HEAD, glm::translate(root, {0, 2.2, 0})); // Head
 
-        // Limb Animation (Sine-based walk cycle)
-        float swing = sin(anim) * 0.5f;
-        dr(vL, N_LIMB, glm::translate(root, {-0.5, 1.5, swing}) * glm::rotate(glm::mat4(1), swing, {1,0,0})); // Left Arm
-        dr(vL, N_LIMB, glm::translate(root, { 0.5, 1.5, -swing}) * glm::rotate(glm::mat4(1), -swing, {1,0,0})); // Right Arm
+        // Walk Cycle (Sine wave based on thumbstick input)
+        float walkSwing = sin(animTime) * 0.5f;
         
-        // Sword (Attached to Hand)
-        dr(vS, N_SWORD, glm::translate(root, {0.6, 1.2, -swing}) * glm::rotate(glm::mat4(1), 1.5f, {1,0,0}));
-        // Shield (Attached to Arm)
-        dr(vSh, N_SHIELD, glm::translate(root, {-0.7, 1.5, swing}));
+        // Left Arm (Shield)
+        glm::mat4 lArm = glm::translate(root, {-0.5, 1.5, walkSwing}) * glm::rotate(glm::mat4(1), walkSwing, {1,0,0});
+        dr(vL, N_LIMB, lArm);
+        dr(vSh, N_SHIELD, glm::translate(lArm, {-0.2, 0.0, 0.2})); 
+
+        // Right Arm (Sword + Attack Animation)
+        float rightArmRot = -walkSwing - (sin(attackSwing) * 2.0f); // Combine walk and attack arcs
+        glm::mat4 rArm = glm::translate(root, {0.5, 1.5, -walkSwing}) * glm::rotate(glm::mat4(1), rightArmRot, {1,0,0});
+        dr(vL, N_LIMB, rArm);
+        dr(vS, N_SWORD, glm::translate(rArm, {0.1, -0.4, 0.5}) * glm::rotate(glm::mat4(1), 1.57f, {1,0,0})); 
     }
 
     JNIEXPORT void JNICALL Java_com_game_procedural_GameLib_onChanged(JNIEnv* e, jclass c, int w, int h) {
