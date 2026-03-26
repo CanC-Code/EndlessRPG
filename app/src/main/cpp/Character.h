491a13d0bda0a57fc107eba808f493053fd06429
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-25 23:36:01 -0600

    Update Character.h

--- app/src/main/cpp/Character.h
+++ app/src/main/cpp/Character.h
@@ -7,18 +7,17 @@ class Character {
 public:
     Character();
     void init();
-    // Dynamically infers walking animation based on positional changes
-    void render(const float* vp, float x, float y, float z, float yaw);
+    void render(const float* vp, float x, float y, float z, float yaw, float cameraX, float cameraZ);
 
 private:
     GLuint vao, vbo, program;
-    
-    // Animation Tracking
     float lastX, lastZ;
     float walkPhase;
     float swingAmplitude;
+    float leanAngle; // Forward tilt based on speed
+    float bankAngle; // Sideways tilt based on turning
 
-    // Matrix Math Utilities for Hierarchical Bones
+    // Matrix Math
     void makeIdentity(float* m);
     void matMul(float* out, const float* a, const float* b);
     void copyMat(float* dst, const float* src);
@@ -28,6 +27,6 @@ private:
     void rotateZLocal(float* m, float angleRad);
     void scaleLocal(float* m, float x, float y, float z);
 
-    // Renders a single anatomical segment
-    void drawSegment(const float* vp, const float* model, float r, float g, float b);
+    // DRAWING SYSTEM: Start radius, end radius, and length for anatomical tapering
+    void drawAnatomicalSegment(const float* vp, const float* model, float rStart, float rEnd, float colorR, float colorG, float colorB, float camX, float camZ);
 };
