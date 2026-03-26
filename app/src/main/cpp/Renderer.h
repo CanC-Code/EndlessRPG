1e0797fb57346c5944edeb166bbfa199da28ac28
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-26 11:40:59 -0600

    Update Renderer.h

--- app/src/main/cpp/Renderer.h
+++ app/src/main/cpp/Renderer.h
@@ -1,39 +1,44 @@
 #pragma once
 #include <GLES3/gl31.h>
-#include <string>
 #include "Character.h"
+#include <string>
+#include <vector>
+
+#define GRASS_COUNT 65536 // 64k blades for photographic density
 
 class GrassRenderer {
 public:
     GrassRenderer();
     void init();
-    void updateAndRender(float time, float dt, int width, int height);
     void updateInput(float mx, float my, float lx, float ly, bool tp, float zoom);
+    void updateAndRender(float time, float dt, int width, int height);
 
 private:
-    GLuint compileShader(GLenum type, const std::string& source);
-    GLuint createProgram(GLuint vShader, GLuint fShader);
-    GLuint createComputeProgram(GLuint cShader);
-    void generateTerrainGrid();
-    float getElevation(float x, float z);
-    void buildPerspective(float* m, float fov, float aspect, float zn, float zf);
-    void buildLookAt(float* m, float ex, float ey, float ez, float cx, float cy, float cz);
-    void multiply(float* out, const float* a, const float* b);
-
-    GLuint computeProgram, renderProgram, terrainProgram;
-    GLuint ssbo, vao, vbo;
+    GLuint renderProgram, terrainProgram, computeProgram;
+    GLuint vao, vbo, instanceVbo, ssbo;
     GLuint terrainVao, terrainVbo, terrainEbo;
     int terrainIndexCount;
 
     Character playerModel;
 
-    float playerX, playerY, playerZ;
-    float playerYaw; // NEW: Independent body rotation
-    float camX, camY, camZ;
-    float camYaw, camPitch;
-    float moveX, moveY;
+    // Transformation State
+    float playerX, playerY, playerZ, playerYaw;
+    float camX, camY, camZ, camYaw, camPitch;
+    float moveX, moveY, cameraZoom;
     bool isThirdPerson;
-    float cameraZoom;
 
-    const int GRASS_COUNT = 60000;
+    // --- NEW PHYSICS & INTERACTION STATE ---
+    float velocityX, velocityZ;   // Momentum for "weighted" movement
+    float smoothPitch, smoothRoll; // Prevents "snapping" on slopes
+    
+    // Internal Utilities
+    float getElevation(float x, float z);
+    void generateTerrainGrid();
+    GLuint compileShader(GLenum type, const std::string& source);
+    GLuint createProgram(GLuint vS, GLuint fS);
+    GLuint createComputeProgram(GLuint cS);
+    
+    void buildPerspective(float* m, float fov, float aspect, float zn, float zf);
+    void buildLookAt(float* m, float ex, float ey, float ez, float cx, float cy, float cz);
+    void multiply(float* out, const float* a, const float* b);
 };
