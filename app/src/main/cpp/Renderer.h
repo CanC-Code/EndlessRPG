51b5a2be4e882d98ac670828c438660ee6cbc5e7
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-29 19:06:54 -0600

    Update Renderer.h

--- app/src/main/cpp/Renderer.h
+++ app/src/main/cpp/Renderer.h
@@ -1,42 +1,42 @@
-#pragma once
+#ifndef RENDERER_H
+#define RENDERER_H
 
 #include <GLES3/gl31.h>
-#include <vector>
-#include <string>
+#include <android/asset_manager.h>
 
 class GrassRenderer {
 public:
     GrassRenderer();
     ~GrassRenderer();
-    
+
     void init();
-    void render(int width, int height);
     
-    // NEW: Added this method to satisfy RenderLoop.cpp
-    void updateAndRender(float time, float dt, int width, int height);
+    // Updated: Now accepts the AssetManager to facilitate loading external shader files
+    void updateAndRender(float time, float dt, int width, int height, AAssetManager* assetManager);
     
     void updateInput(float mx, float my, float lx, float ly, bool tp, float zoom);
 
 private:
+    void render(int width, int height);
+    
+    // Updated: setupShaders now requires the AssetManager to pull files from the assets folder
+    void setupShaders(AAssetManager* assetManager);
+    
     void generateTerrainGrid();
-    void setupShaders();
     
-    // OpenGL Objects
+    // NEW: Helper function to read shader source code from the Android assets directory
+    char* loadShaderFile(AAssetManager* assetManager, const char* filename);
+
+    // OpenGL Handles
     GLuint terrainVAO, terrainVBO, terrainEBO;
-    GLuint terrainProgram;
-    GLuint grassProgram, grassComputeProgram;
+    GLuint terrainProgram, grassProgram, grassComputeProgram;
     GLuint grassSSBO;
-    
-    // Camera & Input State
-    float cameraX = 0.0f;
-    float cameraY = 15.0f;
-    float cameraZ = 0.0f;
-    float camYaw = 0.0f;
-    float camPitch = -0.5f;
-    float moveX = 0.0f;
-    float moveY = 0.0f;
-    float cameraZoom = 15.0f;
-    bool isThirdPerson = true;
-    
     int indexCount;
+
+    // Camera and Movement State
+    float cameraX, cameraY, cameraZ;
+    float camYaw, camPitch;
+    float moveX, moveY;
 };
+
+#endif
