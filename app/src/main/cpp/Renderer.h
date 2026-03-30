872be7fd6e6d5666c2c3b197574e4c53fb0a6c79
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-29 22:43:34 -0600

    Update Renderer.h

--- app/src/main/cpp/Renderer.h
+++ app/src/main/cpp/Renderer.h
@@ -3,37 +3,32 @@
 
 #include <GLES3/gl31.h>
 #include <android/asset_manager.h>
+#include "Character.h" // ADDED
 
 class GrassRenderer {
 public:
     GrassRenderer();
     ~GrassRenderer();
 
-    void init();
-    
-    // Updated: Now accepts the AssetManager to facilitate loading external shader files
     void updateAndRender(float time, float dt, int width, int height, AAssetManager* assetManager);
-    
     void updateInput(float mx, float my, float lx, float ly, bool tp, float zoom);
 
 private:
     void render(int width, int height);
-    
-    // Updated: setupShaders now requires the AssetManager to pull files from the assets folder
     void setupShaders(AAssetManager* assetManager);
-    
     void generateTerrainGrid();
-    
-    // NEW: Helper function to read shader source code from the Android assets directory
     char* loadShaderFile(AAssetManager* assetManager, const char* filename);
 
-    // OpenGL Handles
+    // Physics Engine
+    Character playerCharacter; // FIXED: This declaration was missing
+
+    // OpenGL State
     GLuint terrainVAO, terrainVBO, terrainEBO;
     GLuint terrainProgram, grassProgram, grassComputeProgram;
     GLuint grassSSBO;
     int indexCount;
 
-    // Camera and Movement State
+    // Input state
     float cameraX, cameraY, cameraZ;
     float camYaw, camPitch;
     float moveX, moveY;
